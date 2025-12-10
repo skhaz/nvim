816ccb6f8a4a7bac1cea768f746fa2b65eb62d85
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-10 13:15:24 -0300

    Work in progress

--- init.lua
+++ init.lua
@@ -108,9 +108,15 @@ require("lazy").setup({
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("telescope").setup()
+      require("telescope").setup({
+        defaults = {
+          hidden = true,
+        },
+      })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>ff", function()
+        builtin.find_files({ hidden = true })
+      end, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
