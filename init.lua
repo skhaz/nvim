2067745b85a9af9bae05a087297d3d8ef5afb78a
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-22 11:40:50 -0300

    Work in progress

--- init.lua
+++ init.lua
@@ -33,6 +33,20 @@ if not vim.uv.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local ft = vim.bo.filetype
+    if ft == "markdown" or ft == "diff" or ft == "gitcommit" then
+      return
+    end
+
+    local view = vim.fn.winsaveview()
+    vim.cmd([[silent! keeppatterns %s/\s\+$//e]])
+    vim.fn.winrestview(view)
+  end,
+})
+
 require("lazy").setup({
   {
     "nvim-tree/nvim-tree.lua",
