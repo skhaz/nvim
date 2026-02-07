1c5712ba27fb2edc972e0ea61b053132ac588db6
Rodrigo Delduca <rodrigo@delduca.org>
2026-02-07 18:04:13 -0300

    Work in progress

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.g.mapleader = " "
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -6,10 +7,20 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.autoread = true
--- vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.undofile = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 250
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
 
 vim.diagnostic.config({
   virtual_text = true,
@@ -19,151 +30,46 @@ vim.diagnostic.config({
   severity_sort = true,
 })
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+local augroup = vim.api.nvim_create_augroup("UserAutoGroup", { clear = true })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup,
   pattern = "*",
   callback = function()
-    local ft = vim.bo.filetype
-    if ft == "markdown" or ft == "diff" or ft == "gitcommit" then
+    if vim.tbl_contains({ "markdown", "diff", "gitcommit" }, vim.bo.filetype) then
       return
     end
-
     local view = vim.fn.winsaveview()
     vim.cmd([[silent! keeppatterns %s/\s\+$//e]])
     vim.fn.winrestview(view)
   end,
 })
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup,
+  callback = function()
+    vim.hl.on_yank({ timeout = 200 })
+  end,
+})
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+local rg_base = {
+  "rg", "--color=never", "--no-heading", "--with-filename",
+  "--line-number", "--column", "--smart-case", "--max-columns=0",
+}
+local rg_literal = vim.list_extend(vim.list_extend({}, rg_base), { "--fixed-strings" })
+
 require("lazy").setup({
-  {
-    "nvim-tree/nvim-tree.lua",
-    config = function()
-      local api = require("nvim-tree.api")
-      require("nvim-tree").setup({
-        view = { side = "right" },
-        renderer = {
-          icons = {
-            show = {
-              file = false,
-              folder = false,
-              folder_arrow = false,
-              git = false,
-            },
-          },
-        },
-        on_attach = function(bufnr)
-          api.config.mappings.default_on_attach(bufnr)
-          vim.keymap.set("n", "<C-t>", api.tree.toggle, { buffer = bufnr })
-        end,
-      })
-      vim.keymap.set("n", "<C-t>", api.tree.toggle)
-    end,
-  },
-
-  { "williamboman/mason.nvim", opts = {} },
-
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "clangd",
-          "lua_ls",
-          "pyright",
-          "ts_ls",
-          -- "gopls",
-          "bashls",
-          "jsonls",
-          "yamlls",
-        },
-        automatic_enable = true,
-      })
-    end,
-  },
-
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-nvim-lsp", "neovim/nvim-lspconfig" },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        sources = { { name = "nvim_lsp" } },
-        mapping = cmp.mapping.preset.insert({
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-        }),
-      })
-    end,
-  },
-
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local rg_base = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-        "--max-columns=0"
-      }
-
-      local rg_literal = vim.list_extend({}, rg_base)
-      rg_literal[#rg_literal + 1] = "--fixed-strings"
-
-      require("telescope").setup({
-        defaults = { vimgrep_arguments = rg_literal },
-      })
-
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", function() builtin.find_files({ hidden = true }) end)
-      vim.keymap.set("n", "<C-g>", builtin.live_grep)
-      vim.keymap.set("n", "<C-r>", function() builtin.live_grep({ vimgrep_arguments = rg_base }) end)
-      vim.keymap.set("n", "<leader>fb", builtin.buffers)
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags)
-    end,
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter").setup()
-
-      local langs = { "cpp", "typescript", "python", "lua" }
-      local installed = require("nvim-treesitter").get_installed()
-      local to_install = vim.tbl_filter(function(l)
-        return not vim.list_contains(installed, l)
-      end, langs)
-      if #to_install > 0 then
-        require("nvim-treesitter").install(to_install)
-      end
-
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function()
-          pcall(vim.treesitter.start)
-        end,
-      })
-    end,
-  },
-
   {
     "AlexvZyl/nordic.nvim",
     lazy = false,
@@ -175,30 +81,113 @@ require("lazy").setup({
   },
 
   {
-    "lewis6991/gitsigns.nvim",
+    "nvim-tree/nvim-tree.lua",
     config = function()
-      require("gitsigns").setup({
-        signs = {
-          add          = { text = "+" },
-          change       = { text = "~" },
-          delete       = { text = "-" },
-          topdelete    = { text = "-" },
-          changedelete = { text = "~" },
-          untracked    = { text = "?" },
+      local api = require("nvim-tree.api")
+      require("nvim-tree").setup({
+        view = { side = "right" },
+        renderer = {
+          icons = {
+            show = { file = false, folder = false, folder_arrow = false, git = false },
+          },
         },
+        on_attach = function(bufnr)
+          api.config.mappings.default_on_attach(bufnr)
+          vim.keymap.set("n", "<C-t>", api.tree.toggle, { buffer = bufnr, desc = "Toggle file tree" })
+        end,
+      })
+      vim.keymap.set("n", "<C-t>", api.tree.toggle, { desc = "Toggle file tree" })
+    end,
+  },
+
+  { "williamboman/mason.nvim", opts = {} },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      ensure_installed = {
+        "clangd", "lua_ls", "pyright", "ts_ls",
+        -- "gopls",
+        "bashls", "jsonls", "yamlls",
+      },
+      automatic_enable = true,
+    },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-nvim-lsp", "neovim/nvim-lspconfig" },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        sources = { { name = "nvim_lsp" } },
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"]  = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+        }),
       })
     end,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = { vimgrep_arguments = rg_literal },
+      })
+      local b = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", function() b.find_files({ hidden = true }) end, { desc = "Find files" })
+      vim.keymap.set("n", "<C-g>", b.live_grep, { desc = "Live grep (literal)" })
+      vim.keymap.set("n", "<C-r>", function() b.live_grep({ vimgrep_arguments = rg_base }) end, { desc = "Live grep (regex)" })
+      vim.keymap.set("n", "<leader>fb", b.buffers, { desc = "Find buffers" })
+      vim.keymap.set("n", "<leader>fh", b.help_tags, { desc = "Find help tags" })
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup()
+      local wanted = { "cpp", "typescript", "python", "lua" }
+      local installed = require("nvim-treesitter").get_installed()
+      local missing = vim.tbl_filter(function(l) return not vim.list_contains(installed, l) end, wanted)
+      if #missing > 0 then require("nvim-treesitter").install(missing) end
+      vim.api.nvim_create_autocmd("FileType", {
+        group = augroup,
+        callback = function() pcall(vim.treesitter.start) end,
+      })
+    end,
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add          = { text = "+" },
+        change       = { text = "~" },
+        delete       = { text = "-" },
+        topdelete    = { text = "-" },
+        changedelete = { text = "~" },
+        untracked    = { text = "?" },
+      },
+    },
   },
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
+  group = augroup,
   callback = function(args)
-    local opts = { buffer = args.buf, silent = true }
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    local function map(lhs, rhs, desc)
+      vim.keymap.set("n", lhs, rhs, { buffer = args.buf, silent = true, desc = desc })
+    end
+    map("gd", vim.lsp.buf.definition, "Go to definition")
+    map("K", vim.lsp.buf.hover, "Hover documentation")
+    map("gr", vim.lsp.buf.references, "Find references")
+    map("<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+    map("<leader>ca", vim.lsp.buf.code_action, "Code action")
   end,
 })
-
